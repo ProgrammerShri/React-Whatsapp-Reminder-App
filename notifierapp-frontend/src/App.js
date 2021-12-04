515d29cd43e0c3f8423f60db731c33d068f1d670
programmershri <shrikantdewangan3@gmail.com>
2021-12-05 00:06:40 +0530

    almost done

---
+++ notifierapp-frontend/src/App.js
@@ -0,0 +1,93 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DateTimePicker from "react-datetime-picker";
+
+function App() {
+  const [reminderMsg, setReminderMsg] = useState("");
+  const [remindAt, setRemindAt] = useState();
+  const [reminderList, setReminderList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/getAllReminder")
+      .then((res) => setReminderList(res.data));
+  }, []);
+
+  const addReminder = () => {
+    axios
+      .post("http://localhost:9000/addReminder", { reminderMsg, remindAt })
+      .then((res) => setReminderList(res.data));
+    setReminderMsg("");
+    setRemindAt();
+  };
+
+  const deleteReminder = (id) => {
+    axios
+      .post("http://localhost:9000/deleteReminder", { id })
+      .then((res) => setReminderList(res.data));
+  };
+
+  return (
+    <div className="App">
+      <div className="homepage">
+        <div className="homepage_header">
+          <div className="subHeader">
+          <h1>Remind Me 🙋‍♂️</h1>
+          <input
+          className="inputBox"
+            type="text"
+            placeholder="Reminder Notes Here..."
+            value={reminderMsg}
+            onChange={(e) => setReminderMsg(e.target.value)}
+          />
+          <DateTimePicker
+           className="datePicker"
+            value={remindAt}
+            onChange={setRemindAt}
+            minDate={new Date()}
+            minutePlaceholder="mm"
+            hourPlaceholder="hh"
+            dayPlaceholder="DD"
+            monthPlaceholder="MM"
+            yearPlaceholder="YYYY"
+          />
+          <div className="button" onClick={addReminder}>
+            Add Reminder
+          </div>
+          </div>
+        </div>
+
+        <div className="homepage_body">
+          {reminderList?.map((reminder, i) => (
+            <li>
+              <div className="reminder_card" key={reminder._id}>
+                <div className="msgHead">
+                  {i + 1} - {reminder.reminderMsg}
+                </div>
+                <div className="remindHead">Remind Me at :</div>
+                <div className="remindTime">
+                  {String(
+                    new Date(
+                      reminder?.remindAt?.toLocaleString(undefined, {
+                        timezone: "Asia/Kolkata",
+                      })
+                    )
+                  )}
+                </div>
+                <div
+                  className="button"
+                  onClick={() => deleteReminder(reminder._id)}
+                >
+                  Delete
+                </div>
+              </div>
+            </li>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
